fb6b996571555406b8e962dbfb0668b35b89137c
patton <aoksanaisusanib6175@gmail.com>
2023-05-05 11:46:23 +0800

    Update Reputation.sol

--- contracts/Reputation.sol
+++ contracts/Reputation.sol
@@ -8,6 +8,7 @@ import "./interface/IFireSoul.sol";
 contract Reputation is Ownable {
     mapping(address => uint256) public coefficients;
     address[] public tokens;
+    address[] public subTokens;
     address public fireSoul;
 
     function setFireSoulAddress(address _fireSoul) external onlyOwner {
@@ -19,21 +20,35 @@ contract Reputation is Ownable {
         tokens.push(_token);
         coefficients[_token] = _coefficient;
     }
-
+    function subTokenAddress(address _token, uint256 _coefficient) external onlyOwner{
+        require(coefficients[_token] == 0, "token already exists");
+        subTokens.push(_token);
+        coefficients[_token] = _coefficient;
+        
+    }
     function setCoefficient(address _token, uint256 _coefficient) external onlyOwner {
         require(coefficients[_token] > 0, "token does not exist");
         coefficients[_token] = _coefficient;
     }
-
+    function deleteToken(address _token) external onlyOwner{
+        require(coefficients[_token] > 0, "token does not exist");
+        delete coefficients[_token];
+    }
     function checkReputation(address _user) external view returns (uint256) {
         uint256 reputationPoints;
         for (uint256 i = 0; i < tokens.length; i++) {
             reputationPoints += IERC20(tokens[i]).balanceOf(IFireSoul(fireSoul).getSoulAccount(_user)) * coefficients[tokens[i]];
+        }
+        for(uint256 j = 0; j < subTokens.length;j++){
+            reputationPoints -= IERC20(subTokens[j]).balanceOf(IFireSoul(fireSoul).getSoulAccount(_user)) * coefficients[subTokens[j]];
         }
         return reputationPoints;
     }
 
     function getTokensLength() external view returns (uint256) {
         return tokens.length;
+    }
+    function getSubTokensLength() external view returns(uint256){
+        return subTokens.length;
     }
 }
