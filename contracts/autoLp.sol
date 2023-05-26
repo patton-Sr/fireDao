15f5dd07da7da6f2b90d3d5be38bf5d458109998
patton <aoksanaisusanib6175@gmail.com>
2023-05-26 11:00:41 +0800

    Update autoLp.sol

--- contracts/autoLp.sol
+++ contracts/autoLp.sol
@@ -1,6 +1,4 @@
 
-
-
 // File: contracts/interface/IReputation.sol
 
 pragma solidity ^0.8.0;
@@ -1160,7 +1158,6 @@ abstract contract Ownable is Context {
 //File: contracts/Guild.sol
 
 
-// SPDX-License-Identifier: MIT
 
 /**
  * @title ERC721 token receiver interface
@@ -2817,6 +2814,9 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         _approve(owner, spender, amount);
         return true;
     }
+    
+  
+    
 
     /**
      * @dev See {IERC20-transferFrom}.
@@ -3066,19 +3066,195 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         uint256 amount
     ) internal virtual {}
 }
+// OpenZeppelin Contracts v4.4.1 (token/ERC20/extensions/draft-IERC20Permit.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC20 Permit extension allowing approvals to be made via signatures, as defined in
+ * https://eips.ethereum.org/EIPS/eip-2612[EIP-2612].
+ *
+ * Adds the {permit} method, which can be used to change an account's ERC20 allowance (see {IERC20-allowance}) by
+ * presenting a message signed by the account. By not relying on {IERC20-approve}, the token holder account doesn't
+ * need to send a transaction, and thus is not required to hold Ether at all.
+ */
+interface IERC20Permit {
+    /**
+     * @dev Sets `value` as the allowance of `spender` over ``owner``'s tokens,
+     * given ``owner``'s signed approval.
+     *
+     * IMPORTANT: The same issues {IERC20-approve} has related to transaction
+     * ordering also apply here.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     * - `deadline` must be a timestamp in the future.
+     * - `v`, `r` and `s` must be a valid `secp256k1` signature from `owner`
+     * over the EIP712-formatted function arguments.
+     * - the signature must use ``owner``'s current nonce (see {nonces}).
+     *
+     * For more information on the signature format, see the
+     * https://eips.ethereum.org/EIPS/eip-2612#specification[relevant EIP
+     * section].
+     */
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    /**
+     * @dev Returns the current nonce for `owner`. This value must be
+     * included whenever a signature is generated for {permit}.
+     *
+     * Every successful call to {permit} increases ``owner``'s nonce by one. This
+     * prevents a signature from being used multiple times.
+     */
+    function nonces(address owner) external view returns (uint256);
+
+    /**
+     * @dev Returns the domain separator used in the encoding of the signature for {permit}, as defined by {EIP712}.
+     */
+    // solhint-disable-next-line func-name-mixedcase
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+}
+
+// OpenZeppelin Contracts (last updated v4.8.0) (token/ERC20/utils/SafeERC20.sol)
+
+pragma solidity ^0.8.0;
+
+
+/**
+ * @title SafeERC20
+ * @dev Wrappers around ERC20 operations that throw on failure (when the token
+ * contract returns false). Tokens that return no value (and instead revert or
+ * throw on failure) are also supported, non-reverting calls are assumed to be
+ * successful.
+ * To use this library you can add a `using SafeERC20 for IERC20;` statement to your contract,
+ * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
+ */
+library SafeERC20 {
+    using Address for address;
+
+    function safeTransfer(
+        IERC20 token,
+        address to,
+        uint256 value
+    ) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(
+        IERC20 token,
+        address from,
+        address to,
+        uint256 value
+    ) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    /**
+     * @dev Deprecated. This function has issues similar to the ones found in
+     * {IERC20-approve}, and its usage is discouraged.
+     *
+     * Whenever possible, use {safeIncreaseAllowance} and
+     * {safeDecreaseAllowance} instead.
+     */
+    function safeApprove(
+        IERC20 token,
+        address spender,
+        uint256 value
+    ) internal {
+        // safeApprove should only be called when setting an initial allowance,
+        // or when resetting it to zero. To increase and decrease it, use
+        // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
+        require(
+            (value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+
+    function safeIncreaseAllowance(
+        IERC20 token,
+        address spender,
+        uint256 value
+    ) internal {
+        uint256 newAllowance = token.allowance(address(this), spender) + value;
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    function safeDecreaseAllowance(
+        IERC20 token,
+        address spender,
+        uint256 value
+    ) internal {
+        unchecked {
+            uint256 oldAllowance = token.allowance(address(this), spender);
+            require(oldAllowance >= value, "SafeERC20: decreased allowance below zero");
+            uint256 newAllowance = oldAllowance - value;
+            _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+        }
+    }
+
+    function safePermit(
+        IERC20Permit token,
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) internal {
+        uint256 nonceBefore = token.nonces(owner);
+        token.permit(owner, spender, value, deadline, v, r, s);
+        uint256 nonceAfter = token.nonces(owner);
+        require(nonceAfter == nonceBefore + 1, "SafeERC20: permit did not succeed");
+    }
+
+    /**
+     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+     * on the return value: the return value is optional (but if data is returned, it must not be false).
+     * @param token The token targeted by the call.
+     * @param data The call data (encoded using abi.encode or one of its variants).
+     */
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
+        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
+        // we're implementing it ourselves. We use {Address-functionCall} to perform this call, which verifies that
+        // the target address contains contract code and also asserts for success in the low-level call.
+
+        bytes memory returndata = address(token).functionCall(data, "SafeERC20: low-level call failed");
+        if (returndata.length > 0) {
+            // Return data is optional
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
+}
+
 
 pragma solidity ^0.8.0;
 
 import "./interface/IUniswapV2Router02.sol";
 import "./interface/IUniswapV2Factory.sol";
+
 contract autoLp is Ownable {
+    using SafeERC20 for IERC20;
     FirePassport fp;
-    ERC20 public fdt;
-    ERC20 public weth;
+    address public fdt;
+    address public weth;
     address public  uniswapV2Pair;
     address public reputation;
     address public guarding;
     address public lpReceiver;
+    address private constant UNISWAP_ROUTER_ADDRESS = 0x2863984c246287aeB392b11637b234547f5F1E70;
     IUniswapV2Router02 public uniswapV2Router;
     uint256 immutable public FEE_BASE = 100;
     uint256 public PURCHASE_RATIO;
@@ -3093,9 +3269,9 @@ contract autoLp is Ownable {
     mapping (address => bool) public userPass;
     mapping (address => uint256) public userTime;
 
-    constructor(address _reputation,ERC20 _fdt, ERC20 _weth) {
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x2863984c246287aeB392b11637b234547f5F1E70);
-        
+    constructor(address _reputation,address _fdt) {
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(UNISWAP_ROUTER_ADDRESS);
+        IERC20(_fdt).approve(address(UNISWAP_ROUTER_ADDRESS),100000000 *10 ** 18);
         uniswapV2Router = _uniswapV2Router;
         reputation = _reputation;
         fdt = _fdt;
@@ -3140,46 +3316,34 @@ contract autoLp is Ownable {
     function setuserPass(address _user, bool _set) public onlyOwner {
         userPass[_user] = _set;
     }
-    function buyFdt() internal returns(uint256){
-        uint256 wethAmount = IERC20(uniswapV2Router.WETH()).balanceOf(address(this));
-        if(wethAmount >= 1e18){
-            wethAmount = 1e18;
-        }
-        address[] memory path = new address[](2);
-        path[0] = uniswapV2Router.WETH();
-        path[1] = address(fdt) ;//testnet
-        uniswapV2Router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
-            wethAmount / FEE_BASE * PURCHASE_RATIO,
-            0,
-            path,
-            address(this),
-            block.timestamp
-        );
-        return wethAmount / FEE_BASE *(FEE_BASE - PURCHASE_RATIO);
-    }
-    
-    function autoAddLp() public {
+  function buyAndAddLiquidity() external {
         require(!status,"Contract has been suspended");
         require(IReputation(reputation).checkReputation(msg.sender) >= reputationAmount|| userPass[msg.sender],"You don't have enough reputation points" );
         require(block.timestamp >= contractInterval && block.timestamp >= userTime[msg.sender] || userPass[msg.sender] ,"please come later");
         require(IERC20(uniswapV2Router.WETH()).balanceOf(address(this)) >= min,"Insufficient contract amount");
-        uint256 fdtAmount = IERC20(fdt).balanceOf(address(this));
-        uint256 wethAmount = buyFdt() - reward;
-     
-        contractInterval = block.timestamp + contractIntervals;
-        userTime[msg.sender] = block.timestamp + interval;
-        uniswapV2Router.addLiquidity(
-        address(fdt),
-        uniswapV2Router.WETH(),
-        fdtAmount,
-        wethAmount,
-        0,
-        0,
-        lpReceiver,
-        block.timestamp);
-        TransferHelper.safeTransfer(uniswapV2Router.WETH(), msg.sender, reward);
+        // Buy tokens using half of the available WETH
+        uint wethAmount = address(this).balance / 2;
+        uniswapV2Router.swapExactETHForTokens{value: wethAmount}(
+            0,
+            getPath(),
+            address(this),
+            block.timestamp + 600 // deadline: 10 minutes from now
+        );
 
-        emit record(
+        // Get the token balance
+        uint tokenBalance = IERC20(fdt).balanceOf(address(this));
+        TransferHelper.safeTransfer(uniswapV2Router.WETH(), msg.sender, reward);
+        // Add liquidity to Uniswap
+        IERC20(fdt).safeIncreaseAllowance(UNISWAP_ROUTER_ADDRESS, tokenBalance);
+        uniswapV2Router.addLiquidityETH{value: address(this).balance}(
+            fdt,
+            tokenBalance,
+            0,
+            0,
+            address(this),
+            block.timestamp + 600 // deadline: 10 minutes from now
+        );
+           emit record(
             checkPid(msg.sender),
             checkUsername(msg.sender),
             IReputation(reputation).checkReputation(msg.sender),
@@ -3189,6 +3353,14 @@ contract autoLp is Ownable {
             block.timestamp
             );
     }
+
+    function getPath() private view returns (address[] memory) {
+        address[] memory path = new address[](2);
+        path[0] = uniswapV2Router.WETH();
+        path[1] = fdt;
+        return path;
+    }
+
       function checkPid(address _user) public view returns(uint256){
          (
              uint256 PID,
