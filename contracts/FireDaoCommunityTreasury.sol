2629a2b08d15b5275540ace66b54f6189cae7c84
patton <aoksanaisusanib6175@gmail.com>
2023-06-02 17:32:31 +0800

    Update FireDaoCommunityTreasury.sol

--- contracts/FireDaoCommunityTreasury.sol
+++ contracts/FireDaoCommunityTreasury.sol
@@ -1,45 +1,125 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./interface/IUniswapV2Router02.sol";
-import './lib/TransferHelper.sol';
 
-contract FireDaoCommunityTreasury is Ownable {
+interface WETH {
+    function transfer(address to, uint256 value) external returns (bool);
+}
 
-    IUniswapV2Router02 public uniswapV2Router;
-    address[] public multiWallet;
-    mapping (address => bool) public isNotMulti;
+contract FireDaoCommunityTreasury {
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public numConfirmationsRequired;
+    WETH public wethToken;
 
-    constructor() {
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x2863984c246287aeB392b11637b234547f5F1E70);
-        uniswapV2Router = _uniswapV2Router;
+    struct Transaction {
+        address to;
+        uint value;
+        bool executed;
+        uint numConfirmations;
     }
-    function addmultiWallet(address[] memory _addr) public onlyOwner {
-        for(uint256 i = 0 ; i < _addr.length; i ++){
-            require(_addr[i] != address(0), 'address error');
-            multiWallet.push(_addr[i]);
-            isNotMulti[_addr[i]] = true;
+
+    Transaction[] public transactions;
+    mapping(uint => mapping(address => bool)) public isConfirmed;
+
+    event Deposit(address indexed sender, uint value);
+    event SubmitTransaction(
+        address indexed owner,
+        uint indexed txIndex,
+        address indexed to,
+        uint value
+    );
+    event ConfirmTransaction(address indexed owner, uint indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint indexed txIndex);
+
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "Sender is not an owner");
+        _;
+    }
+
+    modifier txExists(uint _txIndex) {
+        require(_txIndex < transactions.length, "Transaction does not exist");
+        _;
+    }
+
+    modifier notExecuted(uint _txIndex) {
+        require(!transactions[_txIndex].executed, "Transaction already executed");
+        _;
+    }
+
+    modifier notConfirmed(uint _txIndex) {
+        require(!isConfirmed[_txIndex][msg.sender], "Transaction already confirmed");
+        _;
+    }
+
+    constructor(address[] memory _owners, uint _numConfirmationsRequired, address _wethToken) {
+        require(_owners.length > 0, "Owners required");
+        require(
+            _numConfirmationsRequired > 0 && _numConfirmationsRequired <= _owners.length,
+            "Invalid number of required confirmations"
+        );
+
+        for (uint i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+            require(owner != address(0), "Invalid owner");
+            require(!isOwner[owner], "Duplicate owner");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        numConfirmationsRequired = _numConfirmationsRequired;
+        wethToken = WETH(_wethToken);
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function submitTransaction(address _to, uint _value) public onlyOwner {
+        uint txIndex = transactions.length;
+
+        transactions.push(Transaction({
+            to: _to,
+            value: _value,
+            executed: false,
+            numConfirmations: 0
+        }));
+
+        emit SubmitTransaction(msg.sender, txIndex, _to, _value);
+    }
+
+    function confirmTransaction(uint _txIndex) public onlyOwner txExists(_txIndex) notExecuted(_txIndex) notConfirmed(_txIndex) {
+        Transaction storage transaction = transactions[_txIndex];
+
+        isConfirmed[_txIndex][msg.sender] = true;
+        transaction.numConfirmations++;
+
+        emit ConfirmTransaction(msg.sender, _txIndex);
+
+        if (transaction.numConfirmations >= numConfirmationsRequired) {
+            executeTransaction(_txIndex);
         }
     }
-    function deleteAddr(address _addr) internal {
-        for(uint256 i = 0; i< multiWallet.length ;i ++){
-            if(_addr == multiWallet[i]){
-                multiWallet[i] = multiWallet[multiWallet.length - 1];
-                multiWallet.pop();
-                delete isNotMulti[_addr];
-            }
-        }
-    }
-    function removemultiWallet(address[] memory _addr) public onlyOwner {
-        for(uint256 i = 0 ; i < _addr.length ; i++) {
-            require(isNotMulti[_addr[i]],'address error');
-            deleteAddr(_addr[i]);
-        }
-    }
-    function withDraw(uint256 _amount) public {
-        TransferHelper.safeTransfer(uniswapV2Router.WETH(),)
+
+    function executeTransaction(uint _txIndex) public onlyOwner txExists(_txIndex) notExecuted(_txIndex) {
+        Transaction storage transaction = transactions[_txIndex];
+        require(transaction.numConfirmations >= numConfirmationsRequired, "Not enough confirmations");
+
+        transaction.executed = true;
+
+        bool success = wethToken.transfer(transaction.to, transaction.value);
+        require(success, "WETH transfer failed");
+
+        emit ExecuteTransaction(msg.sender, _txIndex);
     }
 
-  
+    function revokeConfirmation(uint _txIndex) public onlyOwner txExists(_txIndex) notExecuted(_txIndex) {
+        Transaction storage transaction = transactions[_txIndex];
+        require(isConfirmed[_txIndex][msg.sender], "Transaction not confirmed by the sender");
+
+        isConfirmed[_txIndex][msg.sender] = false;
+        transaction.numConfirmations--;
+
+        emit RevokeConfirmation(msg.sender, _txIndex);
+    }
 }
