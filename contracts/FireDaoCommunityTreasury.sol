839ff95f58ef4530469df21337d8e7442d03934b
patton <aoksanaisusanib6175@gmail.com>
2023-06-02 16:51:03 +0800

    Update FireDaoCommunityTreasury.sol

--- contracts/FireDaoCommunityTreasury.sol
+++ contracts/FireDaoCommunityTreasury.sol
@@ -3,46 +3,43 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interface/IUniswapV2Router02.sol";
+import './lib/TransferHelper.sol';
 
 contract FireDaoCommunityTreasury is Ownable {
 
     IUniswapV2Router02 public uniswapV2Router;
-    mapping(uint256 => mapping(uint256 => string))public proposal;
-    mapping(uint256 => uint256 ) public tokenAmount;
-    mapping(uint256 => address) public proposalOwner;
-    address public allowAddress;
+    address[] public multiWallet;
+    mapping (address => bool) public isNotMulti;
 
     constructor() {
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x2863984c246287aeB392b11637b234547f5F1E70);
         uniswapV2Router = _uniswapV2Router;
     }
+    function addmultiWallet(address[] memory _addr) public onlyOwner {
+        for(uint256 i = 0 ; i < _addr.length; i ++){
+            require(_addr[i] != address(0), 'address error');
+            multiWallet.push(_addr[i]);
+            isNotMulti[_addr[i]] = true;
+        }
+    }
+    function deleteAddr(address _addr) internal {
+        for(uint256 i = 0; i< multiWallet.length ;i ++){
+            if(_addr == multiWallet[i]){
+                multiWallet[i] = multiWallet[multiWallet.length - 1];
+                multiWallet.pop();
+                delete isNotMulti[_addr];
+            }
+        }
+    }
+    function removemultiWallet(address[] memory _addr) public onlyOwner {
+        for(uint256 i = 0 ; i < _addr.length ; i++) {
+            require(isNotMulti[_addr[i]],'address error');
+            deleteAddr(_addr[i]);
+        }
+    }
+    function withDraw(uint256 _amount) public {
+        TransferHelper.safeTransfer(uniswapV2Router.WETH(),)
+    }
 
-    function setallowAddress(address _allowAddress) public onlyOwner{
-        allowAddress =_allowAddress;
-    }
-    function CreateAProposal (string memory txt, uint256 amount ) public {
-        uint256 id;
-        proposal[id][amount] = txt ;
-        tokenAmount[id] = amount;
-        proposalOwner[id] = msg.sender;
-        id++; 
-
-    }
-    function CreateAProposalExternal (string memory txt, uint256 amount ) external {
-        require(msg.sender == allowAddress, " the address is not allow");
-        uint256 id;
-        proposal[id][amount] = txt ;
-        tokenAmount[id] = amount;
-        proposalOwner[id] = msg.sender;
-        id++; 
-
-    }
-    function Execute(uint256 proposalId) public onlyOwner {
-        IERC20(uniswapV2Router.WETH()).transfer(proposalOwner[proposalId], tokenAmount[proposalId]);
-    }
-
-    function ExecuteExternal(uint256 proposalId) external {
-        require(msg.sender == allowAddress , "the Address is not allow ");
-        IERC20(uniswapV2Router.WETH()).transfer(proposalOwner[proposalId], tokenAmount[proposalId]);
-    }
+  
 }
