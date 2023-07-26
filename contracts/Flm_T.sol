d8bbd13af4e2a1d9098b82e69c549086b5720562
patton <aoksanaisusanib6175@gmail.com>
2023-07-26 17:54:37 +0800

    update

--- contracts/Flm_T.sol
+++ contracts/Flm_T.sol
@@ -4,9 +4,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract flm is ERC20{
   address owner;
-    constructor() ERC20("USDT","USDT"){
+    constructor() ERC20("FLM","FLM"){
         owner =msg.sender;
-        _mint(msg.sender, 10000000000*10**6);
+        _mint(msg.sender, 10000000000000*10**18);
     }
     function mint(uint256 _amount) public  {
         require(msg.sender == owner);
